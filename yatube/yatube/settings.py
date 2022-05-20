fb4f07e4bf74ef95fa604bf9a96ce0e9f9ce1af7
Ilya Petrukhin <Tvules@mail.ru>
2022-05-20 16:32:08 +0400

    Adding django-debug-toolbar.

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'about.apps.AboutConfig',
     'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -57,6 +58,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 CACHES = {
